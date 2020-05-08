48531f6f72cef2889d43ec3b29969d6f893b616b
Ryan Johnson <ryan@258webco.com>
2020-05-08 11:52:03 -0500

    Set up initial assets.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
+import "./default.scss";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Manny!</h1>
     </div>
   );
